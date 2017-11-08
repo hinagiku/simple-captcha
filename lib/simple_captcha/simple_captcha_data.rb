ad1224eb685398328c90af4d73a15a6a1290297a
Liang Chen <holdstock@yeah.net>
2017-11-08 16:23:14 +0800

    fix rails 5.1+ delete_all
    fix rails 5.1+ delete_all

--- lib/simple_captcha/simple_captcha_data.rb
+++ lib/simple_captcha/simple_captcha_data.rb
@@ -17,13 +17,15 @@ module SimpleCaptcha
       end
       
       def remove_data(key)
-        delete_all(["#{connection.quote_column_name(:key)} = ?", key])
+#         delete_all(["#{connection.quote_column_name(:key)} = ?", key])
+        find_by(key: key).destroy
         clear_old_data(1.hour.ago)
       end
       
       def clear_old_data(time = 1.hour.ago)
         return unless Time === time
-        delete_all(["#{connection.quote_column_name(:updated_at)} < ?", time])
+#         delete_all(["#{connection.quote_column_name(:updated_at)} < ?", time])
+        where("update_at < ?", time).each{|r|r.destroy}
       end
     end
   end
